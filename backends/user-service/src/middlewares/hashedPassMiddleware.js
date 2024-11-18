8950c474da34c050bed74bd7cd77ab77396482ad
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-11-19 01:09:52 +0600

    Add: password hashing middleware added

---
+++ backends/user-service/src/middlewares/hashedPassMiddleware.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt');
+
+module.exports = (userSchema) => {
+    // Middleware to hash passwords before saving
+    userSchema.pre('save', async function (next) {
+        if (!this.isModified('password')) return next();
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    });
+
+    // Middleware to hash passwords before updating
+    userSchema.pre('findOneAndUpdate', async function (next) {
+        const update = this.getUpdate();
+        if (update.password) {
+            const hashedPassword = await bcrypt.hash(update.password, 10);
+            this.setUpdate({ ...update, password: hashedPassword });
+        }
+        next();
+    });
+};
