ac4fd19395b9b7a087d185b2aa2c39fab095b784
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-11-19 22:06:39 +0600

    Update: user controller updated

--- backends/user-service/src/controllers/userController.js
+++ backends/user-service/src/controllers/userController.js
@@ -5,7 +5,7 @@ exports.createUser = async (req, res) => {
     try{
         const userData = req.body;
         const newUser = await userService.createUser(userData);
-        res.status(201).json({ message: "User created successfully" }, { user: newUser });
+        res.status(201).json({ message: "User created successfully", user: newUser });
     } catch(error) {
         res.status(500).json({ error: error.message });
     }
