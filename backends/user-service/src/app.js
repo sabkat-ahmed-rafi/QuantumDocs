fa8f64f99e32b81d73890aebac27ddf5a3ae9604
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-11-19 21:49:46 +0600

    Add: create user route added

--- backends/user-service/src/app.js
+++ backends/user-service/src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const userRoutes = require('./routes/userRoutes')
 
 const app = express();
 
+// Middlewares 
 app.use(express.json());
 app.use(cors());
+
+// Routes 
+app.use('/api/users', userRoutes);
 
 
 module.exports = app;
