b2542553f1d09c6d9fa2dd0d74dc9dcf68eff44a
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-11-20 00:05:41 +0600

    Add: get a user by id service added

--- backends/user-service/src/services/userService.js
+++ backends/user-service/src/services/userService.js
@@ -9,3 +9,16 @@ exports.createUser = async (userData) => {
     const user = new User(userData);
     return await user.save();
 }
+
+//get a user by id
+exports.getUserById = async (userId) => {
+    if(!userId) {
+        throw new Error("User id is required");
+    }
+    const user = User.findById(userId);
+
+    if(!user) {
+        throw new Error("User not found");
+    }
+    return user;
+}
