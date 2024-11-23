d55efb1dbcbfee0b5c15551766171b8ced918bd6
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-11-23 14:09:47 +0600

    Add: register page UI created

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,8 +1,36 @@
+'use client'
 import React from 'react'
+import { SiGoogledocs } from 'react-icons/si'
+import { Button, Input } from '@nextui-org/react'
+import Link from 'next/link'
+
 
 const Register = () => {
+
+
   return (
-    <div>Register page bhaya</div>
+    <section>
+    <div className='border p-10 mx-44 my-24 flex justify-between rounded-[15px] shadow-lg shadow-purple-600'>
+        <div className='w-full flex flex-col justify-between'>
+         <div className='space-y-4'>
+         <SiGoogledocs size={45} className="text-purple-500 " />
+         <h1 className='text-4xl font-sans'>Create a Quantum Account</h1>
+         <p className='font-semibold font-sans'>Enter your credentials</p>
+         </div>
+         <p className='italic'>
+            Already have an account? <Link className='text-purple-500' href="/signin">Login</Link>
+         </p>
+        </div>
+        <div className='w-full space-y-6 mt-14'>
+         <Input variant='bordered' size="lg" type="name" placeholder="Name" />
+         <Input variant='bordered' size="lg" type="email" placeholder="Email" />
+         <Input variant='bordered' size="lg" type="password"  placeholder="Password" />
+         <div className='flex justify-end'>
+         <Button className='bg-purple-500 text-white font-bold justify-end'>Sign up</Button>
+         </div>
+        </div>
+    </div>
+    </section>
   )
 }
 
