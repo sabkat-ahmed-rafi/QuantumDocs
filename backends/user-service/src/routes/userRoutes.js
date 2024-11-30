a9e1f58f01493ce2e807f7866c0abce7e6c5691a
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-11-30 17:52:54 +0600

    Add: user get route created

--- backends/user-service/src/routes/userRoutes.js
+++ backends/user-service/src/routes/userRoutes.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/', userController.createUser);
+router.get('/:uid', userController.getUserById);
 
 
 module.exports = router;    
