08954453921e418d8687d6b5938dce08f0caf19d
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-12-02 20:30:35 +0600

    Update: changed the route path of get one user

--- backends/user-service/src/routes/userRoutes.js
+++ backends/user-service/src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/', userController.createUser);
-router.get('/:uid', userController.getUserById);
+router.get('/getUser', userController.getUserById);
 
 
 module.exports = router;    
