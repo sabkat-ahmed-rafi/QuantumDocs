ac745ce82d522daafa019f3cdb81399eafdeda8e
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-12-02 19:48:30 +0600

    Add: subscribe to onAuthStateChanged

--- frontend/app/components/Others/RootLayoutClient.jsx
+++ frontend/app/components/Others/RootLayoutClient.jsx
@@ -1,8 +1,29 @@
-'use client'
-import store from "@/app/store/store"
-import { Provider } from "react-redux"
+'use client';
+import store from "@/app/store/store";
+import { useEffect } from "react";
+import { Provider, useDispatch } from "react-redux";
+import { subscribeToAuthState } from "../../utils/subscribeToAuthState";
 
+function AppContent({ children }) {
+    const dispatch = useDispatch();
 
-export default function RootLayoutClient({children}) {
-    return <Provider store={store}>{children}</Provider>
-} 
+    const getToken = async (email) => {
+        console.log(`Fetching token for ${email}`);
+    };
+
+    useEffect(() => {
+        const unsubscribe = subscribeToAuthState(dispatch, getToken);
+
+        return () => unsubscribe();
+    }, [dispatch]);
+
+    return <>{children}</>;
+}
+
+export default function RootLayoutClient({ children }) {
+    return (
+        <Provider store={store}>
+            <AppContent>{children}</AppContent>
+        </Provider>
+    );
+}
