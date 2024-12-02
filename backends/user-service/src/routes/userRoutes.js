e7f2e465e367a6885ef7b4637eb3f545a1881962
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-12-02 20:40:29 +0600

    Update: userRoute's get route changed

--- backends/user-service/src/routes/userRoutes.js
+++ backends/user-service/src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/', userController.createUser);
-router.get('/getUser', userController.getUserById);
+router.get('/:uid', userController.getUserById);
 
 
 module.exports = router;    
