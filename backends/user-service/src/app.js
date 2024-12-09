31dc7553ce594ccc44a087df7740114ac08798d4
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2024-12-09 22:39:34 +0600

    Add: middleware created to verify token

--- backends/user-service/src/app.js
+++ backends/user-service/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser')
 const config = require('./config/config');
 const userRoutes = require('./routes/userRoutes');
 const jwtRoutes = require('./routes/jwtRoutes')
@@ -8,6 +9,7 @@ const app = express();
 
 // Middlewares 
 app.use(express.json());
+app.use(cookieParser())
 app.use(cors({
         origin: [config.frontend],
         credentials: true,
---
+++ backends/user-service/src/middlewares/verifyToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/config')
+
+
+module.exports = (req, res, next) => {
+    const token = req.cookies?.token;
+    if(!token) {
+        return res.status(401).send({ message: "Unauthorized access" });
+    }
+    jwt.verify(token, config.jwt_secret, (err, decoded) => {
+        if(err) {
+            console.log(err);
+            return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.user = decoded;
+        next();
+    })
+}
