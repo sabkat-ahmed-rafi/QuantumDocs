bfc2f1a473674451b52a0d73e4e88342fd2332a4
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-01-01 16:01:00 +0600

    document-service initialized

---
+++ backends/document-service/src/config/config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+    port: process.env.PORT || 7000,
+    mongodb_uri: process.env.MONGODB_URI || "",
+    frontend: process.env.FRONTEND,
+    jwt_secret: process.env.JWT_SECRET
+}
