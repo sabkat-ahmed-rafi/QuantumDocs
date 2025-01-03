6969e1fa43ee3262371081cc63ca000352ded4a7
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-01-04 02:58:15 +0600

    Update: status code mistake solved

--- backends/document-service/src/controller/documentController.js
+++ backends/document-service/src/controller/documentController.js
@@ -4,7 +4,7 @@ const documentService = require('../service/documentService');
 exports.create = async (req, res) => {
 	try {
 		const document = await documentService.createDocument(req.body);
-		res.status(201).json({ message: "Document created successfully", document });
+		res.status(200).json({ message: "Document created successfully", document });
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
