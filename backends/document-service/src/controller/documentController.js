1ef15a6fd7dbaff4b0c2e023a24b4dd0a216e05a
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-01-04 00:33:38 +0600

    Add: created  get controller for documents

--- backends/document-service/src/controller/documentController.js
+++ backends/document-service/src/controller/documentController.js
@@ -9,3 +9,13 @@ exports.create = async (req, res) => {
 		res.status(500).json({ message: error.message });
 	}
 };
+
+exports.get = async (req, res) => {
+	try {
+		const documentId = req.params.id
+		const document = await documentService.getDocumentById(documentId);
+		res.status(200).json({ message: "Document found", document });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+}
