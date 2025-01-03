c5b537787b7e0f4fe0764cfed4a998fa09a59cb1
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-01-04 04:30:18 +0600

    Add: create document functionality added

--- frontend/app/components/Ui/AddDocument.jsx
+++ frontend/app/components/Ui/AddDocument.jsx
@@ -1,10 +1,30 @@
 'use client'
+import useAxiosSecureForDS from '@/app/hooks/useAxiosSecureForDS';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 import { FaPlus } from "react-icons/fa6";
+import { useSelector } from 'react-redux';
 
 const AddDocument = () => {
+
+    const {user} = useSelector(state => state.auth);
+    const router = useRouter();
+    const axiosSecure = useAxiosSecureForDS();
+
+    const handleCreateDocument = async () => {
+        try {
+            const response = await axiosSecure.post(`/api/document`, { owner: user?.email });
+            console.log(response);
+
+            const documentId = response?.data?.document?._id;
+            router.push(`/api/document/${documentId}`);
+        } catch(error) {
+            console.log("Failed to create document:", error);
+        }
+    }
+
   return (
-    <div href={"/"} className='border border-slate-200 inline-block py-14 px-10 bg-white rounded-sm hover:border-purple-400'>
+    <div onClick={handleCreateDocument} href={"/"} className='border border-slate-200 inline-block py-14 px-10 bg-white rounded-sm hover:border-purple-400'>
        <FaPlus className='text-purple-600' size={60} />
     </div>
   )
---
+++ frontend/app/hooks/useAxiosSecureForDS.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../slices/authSlice";
+import { toast } from "react-toastify";
+
+
+export const axiosSecure = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_document_service,
+    withCredentials: true,
+});
+
+
+const useAxiosSecureForDS = () => {
+    const dispatch = useDispatch();
+    const router = useRouter();
+    const interceptorRef = useRef(null);
+    const toastShownRef = useRef(false); 
+
+    useEffect(() => {
+        if(!interceptorRef.current) {
+            interceptorRef.current = axiosSecure.interceptors.response.use(
+                (response) => response,
+                async (error) => {
+                    const status = error.response?.status;
+                    const message = error.response?.data?.error || "Unknown Error";
+    
+                    if(status == 401 && message == "sessionExpired") {
+                        if (!toastShownRef.current) {
+                            toastShownRef.current = true;
+                            toast.error("Your session has expired. Please log back in to continue.");
+                            await dispatch(logout()).unwrap();
+                            router.push("/signin");
+                            setTimeout(() => {
+                                toastShownRef.current = false;
+                            }, 3000); 
+                        }
+                    } else if (status == 403) {
+                        console.log("Access denied");
+                    }
+                    return Promise.reject(error);
+                }
+            );
+        }
+
+        return () => {
+            if(interceptorRef.current !== null) {
+                axiosSecure.interceptors.response.eject(interceptorRef.current);
+                interceptorRef.current = null;
+            }
+        }
+
+    }, [router]);
+
+    return axiosSecure;
+}
+
+export default useAxiosSecureForDS
