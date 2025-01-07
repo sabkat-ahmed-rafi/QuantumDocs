f2e4ff05ec6eb6e495c302c05377e5b520372b47
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-01-07 14:39:48 +0600

    Update: used the older version of y-socket to setup ws connection

--- backends/document-service/src/config/webSocket.js
+++ backends/document-service/src/config/webSocket.js
@@ -1,11 +1,11 @@
 const WebSocket = require('ws')
-const yWebSocket = require('y-websocket');
+const { setupWSConnection } = require('y-websocket/bin/utils.js');
 
 
 const setupWebSocket = (server) => {
     const wss = new WebSocket.Server({ server });
     wss.on('connection', (ws, req) => {
-        yWebSocket.setupWSConnection(ws, req);
+        setupWSConnection(ws, req);
     });
 };
 
