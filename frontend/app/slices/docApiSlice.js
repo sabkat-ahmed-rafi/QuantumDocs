a9cb9883706c45694b4c856f98b2698cccea3b8b
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-01-09 13:34:58 +0600

    Update: document api slice updated

--- frontend/app/slices/docApiSlice.js
+++ frontend/app/slices/docApiSlice.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 
 export const docApiSlice = createApi({
-    reducerPath: 'documentApi',
+    reducerPath: 'docApiSlice',
     baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_document_service}),
     tagTypes: ['DocumentData'], 
     endpoints: (builder) => ({
