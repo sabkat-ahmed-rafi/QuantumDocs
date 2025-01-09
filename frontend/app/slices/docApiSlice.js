03cba3c65c30529f0c0f04df750feacf76f18d02
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-01-09 12:16:00 +0600

    Add: document api slice created

---
+++ frontend/app/slices/docApiSlice.js
@@ -0,0 +1,49 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+
+export const docApiSlice = createApi({
+    reducerPath: 'documentApi',
+    baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_document_service}),
+    tagTypes: ['DocumentData'], 
+    endpoints: () => ({
+        getAllData: builder.query({
+            query: () => "/api/document",
+            providesTags: ['DocumentData']
+        }),
+        getSingleData: builder.query({
+            query: (documentId) => `/api/document/${documentId}`,
+            providesTags: ['DocumentData']
+        }),
+        addData: builder.mutation({
+            query: (newDocumentData) => ({
+                url: `/api/document`,
+                method: 'POST',
+                body: newDocumentData
+            }),
+            invalidatesTags: ['DocumentData']
+        }),
+        updateData: builder.mutation({
+            query: ({documentId, updatedData}) => ({
+                url: `/api/document/${documentId}`,
+                method: 'PUT',
+                body: updatedData
+            }),
+            providesTags: ['DocumentData']
+        }),
+        deleteData: builder.mutation({
+            query: (documentId) => ({
+                url: `/api/document/${documentId}`,
+                method: 'DELETE'
+            }),
+            providesTags: ['DocumentData']
+        }),
+    }),
+});
+
+export const {
+    useGetAllDataQuery,
+    useGetSingleDataQuery,
+    useAddDataMutation,
+    useUpdateDataMutation,
+    useDeleteDataMutation
+} = docApiSlice;
