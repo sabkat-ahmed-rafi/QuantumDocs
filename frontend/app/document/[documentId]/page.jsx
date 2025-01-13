8b71e50ed8cab53818dc2b8a1aa17751bb924046
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-01-14 03:07:17 +0600

    Update: showing the data in the editor from database

--- frontend/app/document/[documentId]/page.jsx
+++ frontend/app/document/[documentId]/page.jsx
@@ -1,30 +1,115 @@
 'use client'
+
 import { useParams } from 'next/navigation'
 import React from 'react'
 import { useGetSingleDataQuery } from '@/app/slices/docApiSlice';
 import { useSelector } from 'react-redux';
+import { useEffect, useRef, useMemo, useCallback } from 'react';
+import { WebsocketProvider } from 'y-websocket';
+import * as Y from 'yjs';
+import { QuillBinding } from 'y-quill';
+import QuillCursors from 'quill-cursors';
+import Quill from 'quill';
+import katex from "katex";
+
 
 import 'quill/dist/quill.snow.css'
 import "katex/dist/katex.min.css"; 
-import useQuillEditor from '@/app/hooks/useQuillEditor';
 
 
 
 
 const Document = () => {
 
-
   
   const {documentId} = useParams();
   const {user} = useSelector(state => state.auth);
   const {data: document, isLoading} = useGetSingleDataQuery(documentId);
+
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
+  
+  const colors = useMemo(() => ['#FF5733', '#33FF57', '#3357FF', '#F333FF', '#FF3383', '#33FFF1'], []);
+  const getRandomColor = useCallback(() => colors[Math.floor(Math.random() * colors.length)], [colors]);
+  
   
   console.log(document?.document?.state);
-  
-  const { editorRef } = useQuillEditor({documentId, user, document, isLoading});
 
 
-  
+  useEffect(() => {
+    if (!document || isLoading) return;
+
+    window.katex = katex; // katex is used to use the fucntion editing feature.
+    Quill.register('modules/cursors', QuillCursors)
+
+    const ydoc = new Y.Doc();
+    const ytext = ydoc.getText('quill');
+    const provider = new WebsocketProvider(`${process.env.NEXT_PUBLIC_socket_server}`, documentId, ydoc);
+
+    provider.on('status', (event) => {
+      console.log(`WebSocket connection: ${event.status}`);
+    });
+
+    // Editor settings and Quillbinding with Y.js
+    if (editorRef.current && !quillRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: 'snow',
+        modules: {
+          history: { userOnly: true },
+          cursors: true,
+          toolbar: [
+            [{ font: [] }, { header: [1, 2, 3, 4, 5, false] }],
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block'],
+            [{ color: [] }, { background: [] }],
+            [{ script: 'sub' }, { script: 'super' }],
+            [{ indent: '-1' }, { indent: '+1' }],
+            [{ direction: 'rtl' }],
+            [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }, { align: [] }],
+            ['link', 'image', 'video', 'formula'],
+            ['clean'],
+          ],
+        },
+      });
+
+      new QuillBinding(ytext, quillRef.current, provider.awareness);
+
+      provider.awareness.setLocalStateField('user', {
+        name: `${user?.displayName}` || "Anonymous",
+        color: getRandomColor(),
+      });
+    }
+
+     // Showing the data from database 
+     if (document?.document?.state) {
+      const serverState = new Uint8Array(document.document.state.data);
+      const decoded = new TextDecoder().decode(serverState);
+      const delta = JSON.parse(decoded);
+
+      if (delta && delta.ops) { 
+        provider.on('synced', () => {
+          const currentContent = ytext.toDelta();
+    
+            // Check if current content matches the server state
+            console.log("first print")
+            ytext.delete(0, ytext.length); // Clear existing content
+            ytext.applyDelta(delta.ops); // Apply the server state
+            quillRef.current.setContents(delta.ops) // Again setting content because data not showing after route back
+          console.log("Current Content after Sync:", currentContent);
+        });
+      };
+     };
+
+ 
+    return () => {
+      provider.disconnect();
+      ydoc.destroy();
+      editorRef.current = null;
+    };
+  }, [documentId, document, isLoading, getRandomColor]);
+
+
+
 
   return (
     <>
@@ -34,13 +119,15 @@ const Document = () => {
           <div className='lg:w-[1000px] mx-auto bg-[#F0F4F9] lg:h-[44px]'>
             <div 
             ref={editorRef}
-            className='text-cursor caret-purple-600 lg:w-[700px] bg-white border border-gray-300 lg:p-10 p-7' 
+            className='text-cursor caret-purple-600 lg:w-[700px] bg-white border border-gray-300 lg:p-10 md:p-7 p-4' 
             style={{marginLeft: "auto", marginRight: "auto", height: "850px"}}
             >
             </div>
           </div>
         </section>
         </div>
+
+        
         <style jsx global>{`
           body {
             cursor: url('/arrow.svg') 5 5, auto; 
