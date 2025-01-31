cf24bf19db59f9a1d7b57116cb1dcf5733c58ccb
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-02-01 00:56:25 +0600

    Update: Document loading UI changed

--- frontend/app/document/[documentId]/loading.jsx
+++ frontend/app/document/[documentId]/loading.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Image from "next/image";
-import LoadingSVG from '../../../public/bouncingSquares.svg';
 
 const loading = () => {
   return (
     <div className="flex items-center justify-center h-screen text-black bg-white">
-        <Image src={LoadingSVG} alt="Loading..." width={350} height={350} />
+        <Image src={'/bouncingSquares.svg'} alt="Document Loading..." width={350} height={350} priority />
   </div>
   )
 }
