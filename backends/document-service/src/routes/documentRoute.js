a844439ebbd54e0974f8882cc77db7f197c87943
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-02-02 23:51:06 +0600

    Update: verify token middleware removed

--- backends/document-service/src/routes/documentRoute.js
+++ backends/document-service/src/routes/documentRoute.js
@@ -1,10 +1,9 @@
 const documentController = require('../controller/documentController');
-const verifyToken = require('../middleware/verifyToken');
 const express = require('express');
 const router = express.Router();
 
 
-router.post('/', verifyToken, documentController.create);
+router.post('/', documentController.create);
 router.get('/:id', documentController.get);
 router.put('/:id', documentController.update);
 router.patch('/updateTitle/:id', documentController.updateTitle);
