766a331ecc809e89b0698860037a1a29ac0e903e
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-02-05 22:34:35 +0600

    Update: dockerfile updated

--- backends/document-service/src/config/config.js
+++ backends/document-service/src/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    port: process.env.PORT || 7000,
+    port: process.env.PORT || 5002,
     mongodb_uri: process.env.MONGODB_URI || "",
     frontend: process.env.FRONTEND,
     jwt_secret: process.env.JWT_SECRET
--- backends/user-service/src/config/config.js
+++ backends/user-service/src/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || 5001,
     mongodb_uri: process.env.MONGODB_URI || "",
     frontend: process.env.FRONTEND,
     jwt_secret: process.env.JWT_SECRET
