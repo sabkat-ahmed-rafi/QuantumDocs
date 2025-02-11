e1a86a58ce2799d6f44c6939bc69eb3e9c0f0d9e
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-02-11 21:50:04 +0600

    Add: added a loading UI while creating doc

--- frontend/app/components/Ui/AddDocument.jsx
+++ frontend/app/components/Ui/AddDocument.jsx
@@ -1,29 +1,45 @@
 'use client'
 import { useAddDataMutation } from '@/app/slices/docApiSlice';
 import { useRouter } from 'next/navigation';
-import React from 'react'
+import React, { useState } from 'react'
 import { FaPlus } from "react-icons/fa6";
 import { useSelector } from 'react-redux';
+import { quantum } from 'ldrs'
+
+
+
 
 const AddDocument = () => {
 
     const {user} = useSelector(state => state.auth);
     const router = useRouter();
     const [addData] = useAddDataMutation()
+    const [isLoading, setIsLoading] = useState(false);
+    quantum.register()
 
     const handleCreateDocument = async () => {
+      if(isLoading) return;
+      setIsLoading(true); 
         try {
             const response = await addData( { owner: { email: user?.email } } ).unwrap();
             const documentId = response?.document?._id;
-            router.push(`/document/${documentId}`);
+            setTimeout(() => {
+              router.push(`/document/${documentId}`);
+              setIsLoading(false); // Reset loading after navigation
+            }, 2000);
         } catch(error) {
+            setIsLoading(false); 
             console.log("Failed to create document:", error);
         }
     }
 
   return (
     <div onClick={handleCreateDocument} href={"/"} className='border border-slate-200 inline-block py-14 px-10 bg-white rounded-sm hover:border-purple-400 cursor-pointer'>
-       <FaPlus className='text-purple-600' size={60} />
+      {isLoading ? <l-quantum
+          size="60"
+          speed="1.75" 
+          color="purple" 
+        ></l-quantum> : <FaPlus className='text-purple-600' size={60} />}
     </div>
   )
 }
