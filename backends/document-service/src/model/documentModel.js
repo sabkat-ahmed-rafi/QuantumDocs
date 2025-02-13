05ab957a13f0f50812cf51d95f7f8cabdaa22168
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-02-13 19:41:08 +0600

    Update: document model's owner property updated

--- backends/document-service/src/model/documentModel.js
+++ backends/document-service/src/model/documentModel.js
@@ -18,6 +18,9 @@ const documentSchema = new mongoose.Schema({
         },
         photo: {
             type: String,
+        },
+        name: {
+            type: String,
         }
     },
     sharedPersons: [{
