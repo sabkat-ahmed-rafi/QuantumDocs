a4209c1c0972ec90e154f392044e00f1eafc3de7
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-02-15 16:43:29 +0600

    Add: add the route to the router

--- backends/user-service/src/routes/userRoutes.js
+++ backends/user-service/src/routes/userRoutes.js
@@ -4,6 +4,7 @@ const verifyToken = require('../middlewares/verifyToken')
 const router = express.Router();
 
 router.post('/', userController.createUser);
+router.get('/search', userController.searchUsers);
 router.get('/:uid', verifyToken, userController.getUserById);
 
 
