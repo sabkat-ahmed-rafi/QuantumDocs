5eada5b133e315bc7c907f750be4a9d4d4962b74
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-02-16 23:32:47 +0600

    Update: showing the shared users in UI

--- frontend/app/components/Ui/PeopleWithAccess.jsx
+++ frontend/app/components/Ui/PeopleWithAccess.jsx
@@ -10,6 +10,7 @@ const PeopleWithAccess = ({document, peopleWhoHaveAccessRole, handlePeopleWhoHav
     const [isHovered, setIsHovered] = useState(false);
 
     const toggleHover = () => setIsHovered(!isHovered);
+    console.log(document.document.sharedPersons)
 
   return (
     <>
@@ -26,41 +27,44 @@ const PeopleWithAccess = ({document, peopleWhoHaveAccessRole, handlePeopleWhoHav
                   <p className='text-slate-400 text-sm'>Owner</p>       
                 </div>
                   {/* Other User */}
-                <div
-                onClick={toggleHover} // Works for both touch and desktop
-                onTouchStart={toggleHover}                
-                className='flex justify-between items-center py-2 rounded-md transition-all'>
-                <section className='flex justify-center items-center space-x-2 relative overflow-hidden'>
                 {
-                    isHovered && <button
-                    
-                    onClick={(e) =>{
-                        e.stopPropagation();
-                        handleDeleteAccess(e, userUid)}
-                        }
-                    className='animate__animated animate__fadeInRight hover:bg-slate-200 p-2 rounded-full cursor-pointer transition-all'
+                 document && document?.document?.sharedPersons?.map(user => <div
+                 key={user._id}
+                    onClick={toggleHover} // Works for both touch and desktop
+                    onTouchStart={toggleHover}                
+                    className='flex justify-between items-center py-2 rounded-md transition-all'>
+                    <section className='flex justify-center items-center space-x-2 relative overflow-hidden'>
+                    {
+                        isHovered && <button
+                        
+                        onClick={(e) =>{
+                            e.stopPropagation();
+                            handleDeleteAccess(e, user._id)}
+                            }
+                        className='animate__animated animate__fadeInRight hover:bg-slate-200 p-2 rounded-full cursor-pointer transition-all'
+                        >
+                         <MdDelete className='text-lg' />
+                        </button>
+                    }
+                    <User
+                    className='transition-all ease-soft-spring'
+                      avatarProps={{
+                        src: `${user.photo}`,
+                      }}
+                      description={`${user.email}`}
+                      name={`${user.name}`}
+                      />
+                    </section>      
+                    <select 
+                     className='hover:bg-slate-100 transition-all p-2 rounded-md focus:outline-none cursor-pointer relative focus:translate-y-[-50%]' 
+                     defaultValue={peopleWhoHaveAccessRole} 
+                     onChange={(e) => handlePeopleWhoHaveAccessRole(e, userUid)}
                     >
-                     <MdDelete className='text-lg' />
-                    </button>
+                      <option className='bg-white py-2' value="Viewer">Viewer</option>
+                      <option className='bg-white' value="Editor">Editor</option>
+                    </select>
+                    </div>)
                 }
-                <User
-                className='transition-all ease-soft-spring'
-                  avatarProps={{
-                    src: `${document?.document?.owner?.photo}`,
-                  }}
-                  description={`${document?.document?.owner?.email}`}
-                  name={`${document?.document?.owner?.name}`}
-                  />
-                </section>      
-                <select 
-                 className='hover:bg-slate-100 transition-all p-2 rounded-md focus:outline-none cursor-pointer relative focus:translate-y-[-50%]' 
-                 defaultValue={peopleWhoHaveAccessRole} 
-                 onChange={(e) => handlePeopleWhoHaveAccessRole(e, userUid)}
-                >
-                  <option className='bg-white py-2' value="Viewer">Viewer</option>
-                  <option className='bg-white' value="Editor">Editor</option>
-                </select>
-                </div>
             </section>
     </>
   )
