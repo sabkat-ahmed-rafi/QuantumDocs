1516e27095e2a5c1f9781fd7596e3068d6ddc7d8
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-02-18 19:35:34 +0600

    Add: route created for change document role

--- backends/document-service/src/routes/documentRoute.js
+++ backends/document-service/src/routes/documentRoute.js
@@ -8,6 +8,7 @@ router.patch('/giveAccess', documentController.giveAccess);
 router.patch('/giveAccess/giveAccessRole', documentController.giveRoleToAccessibleUser);
 router.delete('/giveAccess/removeAccess', documentController.removeAccess);
 router.patch('/accessStatus/changeAccess', documentController.changeDocumentStatus);
+router.patch('/accessStatus/changeRole', documentController.changeDocumentRole);
 router.get('/:id', documentController.get);
 
 module.exports = router;
