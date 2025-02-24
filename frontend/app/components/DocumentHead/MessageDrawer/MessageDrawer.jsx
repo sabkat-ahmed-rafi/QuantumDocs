474457dbf5b26483979e0e20964d805b96c17847
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-02-24 15:50:13 +0600

    Update: updated the message UI in for document

--- frontend/app/components/DocumentHead/MessageDrawer/MessageDrawer.jsx
+++ frontend/app/components/DocumentHead/MessageDrawer/MessageDrawer.jsx
@@ -7,6 +7,7 @@ import {
     DrawerFooter,
     Button,
     Input,
+    Avatar
   } from "@heroui/react";
 
 const MessageDrawer = ({isOpenMessage, onOpenMessageChange, document}) => {
@@ -26,17 +27,24 @@ const MessageDrawer = ({isOpenMessage, onOpenMessageChange, document}) => {
               <DrawerBody className='text-black'>
                 {/* remote user message */}
                 <div className='mb-1'>
-                  <p className='bg-slate-500 text-white px-4 py-2 rounded-[15px] text-left rounded-bl-[0px] inline-block'>
-                     Click the close button
-                  </p>
-                  <p className='text-sm text-slate-400 text-left'>12:30 - 30 september</p>
+                  <div className='flex items-end space-x-2'>
+                    <Avatar size="sm" src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+                    <p className='bg-slate-500 text-white px-4 py-2 rounded-[15px] text-left rounded-bl-[0px] inline-block w-[200px]'>
+                       Click the close button dfghssss rtqewrgv 34r tgfqwrfv q4r tqrt qwe twer qwerqw console.error( rwe qwer qwtasdf);
+                       
+                    </p>
+                  </div>
+                  <p className='text-sm text-slate-400 text-left ml-11'>12:30 - 30 september</p>
                 </div>
                 {/* local user message */}
                 <div className='mb-1'>
-                  <p className='bg-purple-500 text-white px-4 py-2 rounded-[15px] text-left rounded-br-[0px] inline-block'>
-                     Click the close button or action button to close the drawer.
-                  </p>
-                  <p className='text-sm text-slate-400 text-right'>12:30 - 30 september</p>
+                  <div className='flex justify-end items-end space-x-2'>
+                    <p className='bg-purple-500 text-white px-4 py-2 rounded-[15px] text-left rounded-br-[0px] inline-block w-[200px]'>
+                       Click
+                    </p>
+                    <Avatar size="sm" src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+                  </div>
+                  <p className='text-sm text-slate-400 text-right mr-11'>12:30 - 30 september</p>
                 </div>
               </DrawerBody>
               <hr />
