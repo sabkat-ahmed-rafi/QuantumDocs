4ae317616c68447c299095c866dcef3494a52d61
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-02-27 22:31:35 +0600

    Add: get all documnet function created for controller

--- backends/document-service/src/controller/documentController.js
+++ backends/document-service/src/controller/documentController.js
@@ -82,3 +82,12 @@ exports.searchDocument = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 }
+
+exports.getAllDocuments = async (req, res) => {
+    try {
+        const documents = await documentService.getAllDocument();
+        res.status(200).json({ documents });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
