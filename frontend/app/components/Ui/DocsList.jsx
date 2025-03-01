f1b1bd349122a8015cd340f11eed8207d501af00
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-03-01 22:19:46 +0600

    Update: optimized the DocsList component

--- frontend/app/components/Ui/DocsList.jsx
+++ frontend/app/components/Ui/DocsList.jsx
@@ -2,25 +2,27 @@ import React from 'react'
 import DocListRaw from './DocListRaw';
 import DocListColumn from './DocListColumn';
 
+// Keeping formatDate function outside of the component so it won't recreated on every render 
+const formatDate = (isoDate) => {
+  const date = new Date(isoDate);
+  return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "2-digit",
+  });
+}
+
 const DocsList = ({ documents, isGrid, deleteDocument }) => {
 
-  const formatDate = (isoDate) => {
-    const date = new Date(isoDate);
-    return date.toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "short",
-        day: "2-digit",
-    });
-  }
-
-  const handleCopyDocumentLink = async (documentId) => {
+  const handleCopyDocumentLink = useCallback(async (documentId) => {
     try {
-      const documentLink = `${process.env.NEXT_PUBLIC_frontend}/document/${documentId}`;
+      const documentLink = `${process.env.NEXT_PUBLIC_FRONTEND}/document/${documentId}`;
       await navigator.clipboard.writeText(documentLink);
     } catch (error) {
       toast.error("Something went wrong");
     }
-  }
+  }, []);
+
 
   return (
     <>
