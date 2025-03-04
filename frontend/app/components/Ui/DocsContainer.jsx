5ad4930a2c60025ae297bb853531d44324c25771
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-03-05 03:24:27 +0600

    Add: lazy loading document functionality built

--- frontend/app/components/Ui/DocsContainer.jsx
+++ frontend/app/components/Ui/DocsContainer.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import FilterHeader from './FilterHeader'
 import DocsList from './DocsList'
 import { toast } from 'react-toastify'
@@ -8,6 +8,7 @@ import axios from 'axios'
 import { useSelector } from 'react-redux'
 import NoDocs from './NoDocs'
 import { useDeleteDataMutation } from '@/app/slices/docApiSlice'
+import { Spinner } from '@heroui/react'
 
 const DocsContainer = () => {
 
@@ -16,22 +17,34 @@ const DocsContainer = () => {
   const [documents, setDocuments] = useState([]);
   const {user} = useSelector(state => state.auth);
   const [ deleteData ] = useDeleteDataMutation()
+  const [lastId, setLastId] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const observerRef = useRef(null);
+  const [hasMore, setHasMore] = useState(true);
 
   const getAllDocuments = async () => {
+
+    if (!hasMore || loading) return;
+
+    setLoading(true);
+
     try {
       const result = await axios.get(
         `${process.env.NEXT_PUBLIC_document_service}/api/document`,
-        { params: { userEmail: user?.email, ownershipFilter } }
+        { params: { userEmail: user?.email, ownershipFilter, lastId } }
       )
-      setDocuments(result.data.documents.documents)
+      console.log(result)
+      if(result.data.documents.success) {
+        setDocuments((prev) => lastId ? [...prev, ...result.data.documents.documents] : result.data.documents.documents);
+        setLastId(result.data.documents.lastId);
+        setLoading(false);
+        setHasMore(result.data.documents.hasMore);
+      }
     } catch (error) {
       toast.error("Something went wrong");
+      setLoading(false)
     }
   }
-
-  useEffect(() => {
-    getAllDocuments()
-  }, [user?.email, ownershipFilter])
 
   const deleteDocument = async (documentId) => {
     try {
@@ -44,12 +57,41 @@ const DocsContainer = () => {
     }
   }
 
+  useEffect(() => {
+    setDocuments([]);
+    setLastId(null);
+    getAllDocuments()
+  }, [user?.email, ownershipFilter])
+
+  useEffect(() => {
+    if(!observerRef.current) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if(entries[0].isIntersecting) {
+          getAllDocuments();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    observer.observe(observerRef.current);
+
+    return () => observer.disconnect();
+
+  }, [lastId])
+
+
   return (
     <>
       <FilterHeader setOwnershipFilter={setOwnershipFilter} ownershipFilter={ownershipFilter} setIsGrid={setIsGrid} isGrid={isGrid} />
       {
-        documents.length == 0 ? <NoDocs /> : <DocsList documents={documents} isGrid={isGrid} deleteDocument={deleteDocument} />
+        documents.length == 0 ? !loading && <NoDocs /> : <DocsList documents={documents} isGrid={isGrid} deleteDocument={deleteDocument} />
       }
+
+      { loading && <Spinner color='secondary' className='flex justify-center items-center text-xl font-semibold'>Loading</Spinner> }
+
+      <div ref={observerRef} style={{ height: "20px" }} />
     </>
   )
 }
