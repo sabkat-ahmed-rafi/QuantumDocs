03fad668f09119c8d5229ef751ffcacfdab2ff41
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-03-22 20:39:22 +0600

    Update: function created to count unread messages

--- backends/communication-service/src/services/messageService.js
+++ backends/communication-service/src/services/messageService.js
@@ -10,8 +10,18 @@ const getMessages = async (groupId, page = 1, limit = 10) => {
     } catch (error) {
         return { success: false, message: "Something went wrong" };
     }
-}
+};
+
+const getUnreadMessagesCount = async (userId, groupId) => {
+    try {
+        const unreadMessageCount = await Message.countDocuments({ groupId, readBy: { $ne: userId } });
+        return { success: true, message: "Message counted successfully", unreadMessageCount };
+    } catch (error) {
+        return { success: false, message: "Something went wrong" };
+    }
+};
 
 module.exports = {
     getMessages,
+    getUnreadMessagesCount
 }
