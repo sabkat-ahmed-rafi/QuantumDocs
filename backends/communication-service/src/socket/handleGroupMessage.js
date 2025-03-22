f74df81167e753650dffe2d025559f01c227c73a
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-03-23 00:04:25 +0600

    Add: created the mark as read functionality

--- backends/communication-service/src/socket/handleGroupMessage.js
+++ backends/communication-service/src/socket/handleGroupMessage.js
@@ -5,13 +5,19 @@ const handleGroupMessage = (io, socket) => {
     socket.on('send-group-message', async ({ sender, groupId, text }) => {
         console.log(sender, groupId, text)
         const message = await Message.create({ text, groupId, sender, readBy: [sender.uid] });
-
+        const senderUid = sender.uid;
+        const count = 1;
         io.to(groupId).emit('receive-group-message', message);
-        io.to(groupId).emit('receive-unread-group-message', 1);
+        io.to(groupId).emit('receive-unread-group-message', { count, senderUid });
     });
 
     socket.on('group-message-mark-as-read', async ({ userId, groupId }) => {
-        await markAsRead(userId, groupId);
+        try {
+            await markAsRead(userId, groupId);
+            return { success: true, message: "Marked as read" };
+        } catch {
+            return { success: false, message: error.message };
+        }
     });
 
     socket.on('join-group-message', (groupId) => {
