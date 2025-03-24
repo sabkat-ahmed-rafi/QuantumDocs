670c7aeeaf86be8c9d63af6adb35a32e804085ba
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-03-24 12:53:11 +0600

    UI: created the video calling UI

--- frontend/app/components/Ui/VideoCallModal.jsx
+++ frontend/app/components/Ui/VideoCallModal.jsx
@@ -1,40 +1,44 @@
+import React, { useState } from 'react'
 import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from '@heroui/react'
-import React from 'react'
+import { MdCallEnd } from "react-icons/md";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
 
 const VideoCallModal = ({ isOpenVideoCall, onOpenChangeVideoCall }) => {
+
+  const [users, setUsers] = useState(["User 1", "User 4", "User 3", "User 2", "User 5"]);
+
+
   return (
     <>
       <Modal size='full' isOpen={isOpenVideoCall} onOpenChange={onOpenChangeVideoCall} >
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-              <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
-                  risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
-                  quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
-                  risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
-                  quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor
-                  adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit
-                  officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa
-                  deserunt nostrud ad veniam.
-                </p>
+              <ModalHeader className="font-extrabold bg-slate-50 font-sans text-2xl flex justify-center">Video Call</ModalHeader>
+              <ModalBody className='bg-slate-50 overflow-y'>
+                <div  
+                className="flex justify-center items-center flex-wrap sm:gap-1"
+                >
+                {users.map((user, index) => (
+                    <video
+                      key={index}
+                      controls
+                      className={`
+                        ${users.length >=5 && "w-40 sm:w-52 xl:w-[400px] h-32 xl:h-[300px] sm:h-48"}
+                        ${users.length == 2 && "w-60 h-48 md:w-80 md:h-96 xl:w-[550px] xl:h-[600px]"}
+                        ${users.length == 3 && "w-60 h-32 md:w-52 md:h-80 xl:w-[450px] xl:h-[600px]"}
+                        ${users.length == 4 && "w-40 h-48 md:w-80 xl:w-[550px] xl:h-[300px]"}
+                        ${users.length == 1 && "h-96 w-60 xl:w-[500px] xl:h-[600px]"}
+                        md:rounded-lg border border-gray-400 object-fit`}
+                      src="/video.mp4"
+                    ></video>
+                  ))}
+                  </div>
+                
               </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
+              <ModalFooter className='flex justify-center bg-slate-50 fixed bottom-0 w-full'>
+                <div className='bg-red-600 p-2 rounded-full cursor-pointer hover:bg-red-500'><MdCallEnd className='text-white' size={40} /></div>
               </ModalFooter>
             </>
           )}
