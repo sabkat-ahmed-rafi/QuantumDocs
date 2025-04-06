95709dbc32c62dca8bdc0fb9671680a945f66f3d
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-04-06 14:33:55 +0600

    Add: event created to check ongoing call

--- backends/communication-service/src/socket/checkOngoingCall.js
+++ backends/communication-service/src/socket/checkOngoingCall.js
@@ -1,19 +1,59 @@
+const GroupCall = require('../models/groupCallSchema');
+
+
 const checkOngoingCall = async (io, socket) => {
 
-  socket.on('start-call', (groupId, calledBy) => {
-    ongoingCalls[groupId] = true;
-    socket.join(groupId);
-    io.to(groupId).emit('call-started', groupId);
+  socket.on('start-call', async (groupId, startedBy) => {
+    try {
+      let call = await GroupCall.findOne({ groupId });
+
+      if (!call) {
+        // No existing call record, create one
+        call = await GroupCall.create({
+          groupId,
+          ongoing: true,
+          startedBy: startedBy,
+        });
+      } else {
+        // Update existing call status to ongoing
+        call.ongoing = true;
+        call.startedBy = startedBy;
+        call.createdAt = Date.now();
+        await call.save();
+      }
+
+      io.to(groupId).emit('call-started', groupId);
+    } catch (error) {
+      console.log('Error starting call:', error);
+    }
+
+
   });
 
-  socket.on('end-call', (groupId) => {
-    delete ongoingCalls[groupId];
-    socket.leave(groupId);
-    io.to(groupId).emit('call-ended', groupId);
+  socket.on('end-call', async (groupId) => {
+  
+    try {
+      await GroupCall.findOneAndUpdate(
+        { groupId },
+        { ongoing: false },
+        { new: true }
+      );
+
+      io.to(groupId).emit('call-ended', groupId);
+    } catch (error) {
+      console.log('Error ending call:', error);
+    }
+
   });
 
-  socket.on('check-call-status', (groupId, callback) => {
-    callback(!!ongoingCalls[groupId]);
+  socket.on('check-call-status', async (groupId, callback) => {
+    try {
+      const call = await GroupCall.findOne({ groupId });
+      callback(call?.ongoing || false);
+    } catch (error) {
+      console.error('Error checking call status:', error);
+      callback(false);
+    }
   });
 };
 
