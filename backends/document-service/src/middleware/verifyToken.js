9fb98aaa516b9168578326a8f6cce17952720717
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-05-03 20:22:27 +0600

    consoling req

--- backends/document-service/src/middleware/verifyToken.js
+++ backends/document-service/src/middleware/verifyToken.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
     
     
     const token = req.cookies?.token;
-    console.log(req.cookies);
+    console.log(req);
     if(!token) {
         return res.status(401).send({ message: "Unauthorized access" });
     }
