afd8647c497afbeab5737b6d8f7fbf29fd6f81a9
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-05-04 11:27:30 +0600

    removed the third bracket

--- backends/user-service/src/app.js
+++ backends/user-service/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser())
 app.use(express.json());
 app.use(cors({
-        origin: [config.frontend],
+        origin: config.frontend,
         credentials: true,
     })
 );
