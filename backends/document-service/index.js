6904fa39cfd0e3fdd72175c2ff14c0ab2443ab79
sabkat-ahmed-rafi <sabkatahmedrafi@gmail.com>
2025-05-05 17:47:56 +0600

    updated the index.js

--- backends/document-service/index.js
+++ backends/document-service/index.js
@@ -3,12 +3,35 @@ const config = require('./src/config/config');
 const connectDB = require('./src/utils/dbConnection');
 const { setupYWebSocket } = require('./src/config/yWebSocket');
 const { setupCustomWebSocket } = require('./src/config/customWebSocket');
+const http = require('http');
 
 const PORT = config.port;
 
 connectDB();
 
-const server = app.listen(PORT, '0.0.0.0', () => {
+const server = http.createServer(app);
+
+// Initialize WebSocket servers
+const yWebSocketServer = setupYWebSocket();       // returns a ws.Server
+const customWebSocketServer = setupCustomWebSocket(); // returns a ws.Server
+
+server.on('upgrade', (request, socket, head) => {
+    const pathname = request.url;
+
+    if (pathname.startsWith('/yjs')) {
+        yWebSocketServer.handleUpgrade(request, socket, head, (ws) => {
+            yWebSocketServer.emit('connection', ws, request);
+        });
+    } else if (pathname === '/custom-ws') {
+        customWebSocketServer.handleUpgrade(request, socket, head, (ws) => {
+            customWebSocketServer.emit('connection', ws, request);
+        });
+    } else {
+        socket.destroy();
+    }
+});
+
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Document service is running on port ${PORT}`);
 })
 
